503055642410f97eb28b2a07f95c91c618212dc7
Shubham Singh <90834170+shbhm-0711@users.noreply.github.com>
2023-12-03 19:21:22 +0530

    fix:data restructure

--- backend/src/fakeData/blogs.js
+++ backend/src/fakeData/blogs.js
@@ -1,170 +1,189 @@
 export default [
-  [
-    {
-      id: 1,
-      content: {
-        type: "text",
-        size: "heading",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+  {
+    id: 1,
+    auhtor: "Shbhm",
+    time: new Date(),
+    tags: ["UI"],
+    blog: [
+      {
+        id: 1,
+        content: {
+          type: "text",
+          size: "heading",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+        },
+        style: ["heading"],
       },
-      style: ["heading"],
-    },
-    {
-      id: 2,
-      content: {
-        type: "img",
-        dimension: "1080x720",
-        src: "https://placehold.co/1080x720/purple/white/webp?text=Placeholder%20Only",
+      {
+        id: 2,
+        content: {
+          type: "img",
+          dimension: "1080x720",
+          src: "https://placehold.co/1080x720/purple/white/webp?text=Placeholder%20Only",
+        },
+        style: ["heading"],
       },
-      style: ["heading"],
-    },
-    {
-      id: 3,
-      content: {
-        type: "text",
-        size: "small",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+      {
+        id: 3,
+        content: {
+          type: "text",
+          size: "small",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+        },
+        style: ["normal"],
       },
-      style: ["normal"],
-    },
-    {
-      id: 4,
-      content: {
-        type: "text",
-        size: "normal",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+      {
+        id: 4,
+        content: {
+          type: "text",
+          size: "normal",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+        },
+        style: ["normal"],
       },
-      style: ["normal"],
-    },
-    {
-      id: 5,
-      content: {
-        type: "text",
-        size: "normal",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+      {
+        id: 5,
+        content: {
+          type: "text",
+          size: "normal",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+        },
+        style: ["em"],
       },
-      style: ["em"],
-    },
-    {
-      id: 6,
-      content: {
-        type: "text",
-        size: "normal",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+      {
+        id: 6,
+        content: {
+          type: "text",
+          size: "normal",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+        },
+        style: ["bold"],
       },
-      style: ["bold"],
-    },
-  ],
-  [
-    {
-      id: 1,
-      content: {
-        type: "text",
-        size: "heading",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+    ],
+  },
+
+  {
+    id: 2,
+    auhtor: "abc",
+    time: new Date(),
+    tags: ["UI", "UX"],
+    blogs: [
+      {
+        id: 1,
+        content: {
+          type: "text",
+          size: "heading",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+        },
+        style: ["heading"],
       },
-      style: ["heading"],
-    },
-    {
-      id: 2,
-      content: {
-        type: "img",
-        dimension: "1080x720",
-        src: "https://placehold.co/1080x720/purple/white/webp?text=Placeholder%20Only",
+      {
+        id: 2,
+        content: {
+          type: "img",
+          dimension: "1080x720",
+          src: "https://placehold.co/1080x720/purple/white/webp?text=Placeholder%20Only",
+        },
+        style: ["heading"],
       },
-      style: ["heading"],
-    },
-    {
-      id: 3,
-      content: {
-        type: "text",
-        size: "small",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+      {
+        id: 3,
+        content: {
+          type: "text",
+          size: "small",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+        },
+        style: ["normal"],
       },
-      style: ["normal"],
-    },
-    {
-      id: 4,
-      content: {
-        type: "text",
-        size: "normal",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+      {
+        id: 4,
+        content: {
+          type: "text",
+          size: "normal",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+        },
+        style: ["normal"],
       },
-      style: ["normal"],
-    },
-    {
-      id: 5,
-      content: {
-        type: "text",
-        size: "normal",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+      {
+        id: 5,
+        content: {
+          type: "text",
+          size: "normal",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+        },
+        style: ["em"],
       },
-      style: ["em"],
-    },
-    {
-      id: 6,
-      content: {
-        type: "text",
-        size: "normal",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+      {
+        id: 6,
+        content: {
+          type: "text",
+          size: "normal",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+        },
+        style: ["bold"],
       },
-      style: ["bold"],
-    },
-  ],
-  [
-    {
-      id: 1,
-      content: {
-        type: "text",
-        size: "heading",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+    ],
+  },
+  {
+    id: 3,
+    auhtor: "abc",
+    time: new Date(),
+    tags: ["3D", "WebGPU"],
+    blogs: [
+      {
+        id: 1,
+        content: {
+          type: "text",
+          size: "heading",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+        },
+        style: ["heading"],
       },
-      style: ["heading"],
-    },
-    {
-      id: 2,
-      content: {
-        type: "img",
-        dimension: "1080x720",
-        src: "https://placehold.co/1080x720/purple/white/webp?text=Placeholder%20Only",
+      {
+        id: 2,
+        content: {
+          type: "img",
+          dimension: "1080x720",
+          src: "https://placehold.co/1080x720/purple/white/webp?text=Placeholder%20Only",
+        },
+        style: ["heading"],
       },
-      style: ["heading"],
-    },
-    {
-      id: 3,
-      content: {
-        type: "text",
-        size: "small",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+      {
+        id: 3,
+        content: {
+          type: "text",
+          size: "small",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+        },
+        style: ["normal"],
       },
-      style: ["normal"],
-    },
-    {
-      id: 4,
-      content: {
-        type: "text",
-        size: "normal",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+      {
+        id: 4,
+        content: {
+          type: "text",
+          size: "normal",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+        },
+        style: ["normal"],
       },
-      style: ["normal"],
-    },
-    {
-      id: 5,
-      content: {
-        type: "text",
-        size: "normal",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+      {
+        id: 5,
+        content: {
+          type: "text",
+          size: "normal",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+        },
+        style: ["bold"],
       },
-      style: ["bold"],
-    },
-    {
-      id: 6,
-      content: {
-        type: "text",
-        size: "normal",
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+      {
+        id: 6,
+        content: {
+          type: "text",
+          size: "normal",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sapiente quis pariatur cumque assumenda voluptatem harum. Eos temporibus dolor rerum, eum pariatur ratione non alias beatae, placeat doloremque ducimus. Corrupti nemo eveniet laboriosam aliquam excepturi, sapiente consectetur sunt beatae aspernatur velit. Quia fugiat maxime ea tempora tempore? Suscipit, nemo alias.",
+        },
+        style: ["em"],
       },
-      style: ["em"],
-    },
-  ],
+    ],
+  },
 ];
