45fe0d85279b1883c3f4611ed6cd1459cec210bd
Shubham Singh <90834170+shbhm-0711@users.noreply.github.com>
2023-12-03 19:09:43 +0530

    feat:basic api integration

--- frontend/src/components/HomePage/Main.jsx
+++ frontend/src/components/HomePage/Main.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Main() {
+  useEffect(() => {
+    fetch(import.meta.env.VITE_BACKEND_ENDPOINT + "/blogs/fake")
+      .then((res) => res.json())
+      .then((resp) => {
+        console.log(resp.data);
+      });
+  }, []);
   return <div>Main</div>;
 }
 
