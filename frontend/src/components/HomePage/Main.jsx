43d94e84e75fa2124eb6de76a917e939d6208ccf
Sujoy Manna <sujoymanna007@gmail.com>
2023-12-08 13:21:47 +0530

    fix:sign up button and feat:redirect based on auth

--- frontend/src/components/HomePage/Main.jsx
+++ frontend/src/components/HomePage/Main.jsx
@@ -1,14 +1,25 @@
 import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import Welcome from "./Welcome";
 
 function Main() {
+  //is login or not
+  const status = useSelector((state) => state.status);
+  const navigate = useNavigate();
   useEffect(() => {
-    fetch(import.meta.env.VITE_BACKEND_ENDPOINT + "/blogs/fake")
-      .then((res) => res.json())
-      .then((resp) => {
-        console.log(resp.data);
-      });
+    if (status) {
+      fetch(import.meta.env.VITE_BACKEND_ENDPOINT + "/blogs/fake")
+        .then((res) => res.json())
+        .then((resp) => {
+          console.log(resp.data);
+        });
+    }
+    //  else {
+    //   navigate("/login");
+    // }
   }, []);
-  return <div>Main</div>;
+  return status ? <div>Main</div> : <Welcome />;
 }
 
 export default Main;
---
+++ frontend/src/components/HomePage/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import welcomeImage from "./../../assets/Welcome.jpeg";
+
+function Welcome() {
+  return (
+    <div className=" text-black h-[80vh] w-full flex justify-center bg-gray-300 items-center">
+      Welcome page
+    </div>
+  );
+}
+
+export default Welcome;
